8df4af81269c0b817f4394a2b95616df13561b10
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-02-23 10:36:23 -0300

    Processing client tick

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Inputs;
+using Network;
 using Unity.Netcode;
 
 namespace Player
@@ -7,6 +9,7 @@ namespace Player
     public class PlayerMovement : NetworkBehaviour
     {
         [SerializeField] private InputReader inputReader;
+        [SerializeField] private Rigidbody rb;
 
         [SerializeField] private float moveSpeed;
         [SerializeField] private float runSpeed;
@@ -18,40 +21,123 @@ namespace Player
         private float _currentSpeed;
 
         
+        // Prediction and Reconciliation
+        private NetworkTimer _networkTimer;
+        private const float SERVER_TICK_RATE = 60f;
+        private const int BUFFER_SIZE = 1024;
+
+        private Buffer<InputPayload> _clientInputBuffer;
+        private Buffer<StatePayload> _clientStateBuffer;
+        
+        private StatePayload _lastServerState;
+        private StatePayload _lastProcessedState;
+
+        private Buffer<StatePayload> _serverStateBuffer;
+        private Queue<InputPayload> _serverInputQueue;
+        
+        
+        private void Awake()
+        {
+            _networkTimer = new NetworkTimer(SERVER_TICK_RATE);
+            
+            _clientInputBuffer = new Buffer<InputPayload>(BUFFER_SIZE);
+            _clientStateBuffer = new Buffer<StatePayload>(BUFFER_SIZE);
+            
+            _serverStateBuffer = new Buffer<StatePayload>(BUFFER_SIZE);
+            _serverInputQueue = new Queue<InputPayload>();
+            
+        }
+        
         public override void OnNetworkSpawn()
         {
             if (IsOwner)
             {
                 inputReader.OnMoveEvent += InputReader_OnMoveEvent;
                 inputReader.OnRunEvent += InputReader_OnRunEvent;
+                
             }
             
         }
-
         
-        private void InputReader_OnMoveEvent(Vector2 movementInput)
-        {
-            _movementInput = movementInput;
-        }
+        private void InputReader_OnMoveEvent(Vector2 movementInput) => _movementInput = movementInput;
+        private void InputReader_OnRunEvent(bool isRunning) => _isRunning = isRunning;
         
-        private void InputReader_OnRunEvent(bool isRunning)
-        {
-            _isRunning = isRunning;
-        }
 
         private void Update()
         {
+            _networkTimer.Update(Time.deltaTime);
+            
+        }
+        
+        private void FixedUpdate()
+        {
             if (IsOwner)
             {
-                _movementDirection = transform.TransformDirection(_movementInput.x, 0, _movementInput.y);
+                while (_networkTimer.ShouldTick())
+                {
+                    ProcessClientTick();
+                    ProcessServerTick();
+                }
+            }
+            
+        }
+        
+        private void ProcessClientTick()
+        {
+            if (IsClient)
+            {
+                int currentTick = _networkTimer.CurrentTick;
+                int bufferIndex = currentTick % BUFFER_SIZE;
                 
-                _currentSpeed = _isRunning ? runSpeed : moveSpeed;
-                transform.position += _movementDirection * (_currentSpeed * Time.deltaTime);
+                InputPayload inputPayload = new InputPayload
+                {
+                    Tick = currentTick,
+                    InputVector = _movementInput,
+                };
+                
+                _clientInputBuffer.Add(inputPayload, bufferIndex);
+                
+                SendInputToServerRpc(inputPayload);
+                
+                StatePayload statePayload = ProcessClientMovement(inputPayload);
+                _clientStateBuffer.Add(statePayload, bufferIndex);
                 
             }
             
         }
         
+        [Rpc(SendTo.Server)]
+        private void SendInputToServerRpc(InputPayload input)
+        {
+            _serverInputQueue.Enqueue(input);
+            
+        }
+        
+        private StatePayload ProcessClientMovement(InputPayload input)
+        {
+            Move(input.InputVector);
+            
+            return new StatePayload
+            {
+                Tick = input.Tick,
+                Position = transform.position,
+                Rotation = transform.rotation,
+                Velocity = rb.linearVelocity,
+                AngularVelocity = rb.angularVelocity,
+                
+            };
+            
+        }
+        
+        private void Move(Vector2 inputVector)
+        {
+            
+        }
+        
+        private void ProcessServerTick()
+        {
+            
+        }
         
         public override void OnNetworkDespawn()
         {
@@ -59,6 +145,7 @@ namespace Player
             {
                 inputReader.OnMoveEvent -= InputReader_OnMoveEvent;
                 inputReader.OnRunEvent -= InputReader_OnRunEvent;
+                
             }
             
         }

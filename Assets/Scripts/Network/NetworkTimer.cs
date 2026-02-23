2fb7b092a6e59cdb1ee151c4d095f8830c56fa2b
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-02-23 08:11:26 -0300

    Create network timer for sync client and server ticks

---
+++ Assets/Scripts/Network/NetworkTimer.cs
@@ -0,0 +1,39 @@
+
+namespace Network
+{
+    public class NetworkTimer
+    {
+        public float TimeBetweenTick;
+        public int CurrentTick { get; private set; }
+        
+        private float _timer;
+
+        
+        public NetworkTimer(float serverTickRate)
+        {
+            TimeBetweenTick = 1f /  serverTickRate;
+            
+        }
+
+        public void Update(float deltaTime)
+        {
+            _timer += deltaTime;
+            
+        }
+
+        public bool shouldTick()
+        {
+            if (_timer >= TimeBetweenTick)
+            {
+                _timer -= TimeBetweenTick;
+                CurrentTick++;
+                return true;
+            }
+            
+            return false;
+            
+        }
+        
+    }
+}
+

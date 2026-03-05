b35320eb418de243f5c49a36cacf75a3e68e1769
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-03-05 19:13:35 -0300

    System for modularity in player movement scripts

---
+++ Assets/Scripts/Player/PlayerCrouch.cs
@@ -0,0 +1,50 @@
+using Inputs;
+using Interfaces;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerCrouch : NetworkBehaviour, ISpeedModifier
+    {
+        [SerializeField] private InputReader inputReader;
+        [SerializeField] private float speedModifier = 0.5f;
+    
+        private bool _isCrouching;
+
+        
+        public override void OnNetworkSpawn()
+        {
+            if (IsOwner)
+            {
+                inputReader.OnCrouchEvent += InputReader_OnCrouchEvent;
+                
+            }
+        }
+
+        private void InputReader_OnCrouchEvent(bool isCrouching) => _isCrouching = isCrouching;
+
+        public float ModifySpeed(float baseSpeed)
+        {
+            if (_isCrouching)
+            {
+                return baseSpeed * speedModifier;
+                
+            }
+
+            return baseSpeed;
+            
+        }
+
+
+        public override void OnNetworkDespawn()
+        {
+            if (IsOwner)
+            {
+                inputReader.OnCrouchEvent -= InputReader_OnCrouchEvent;
+                
+            }
+        }
+    }  
+}
+

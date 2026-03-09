dd465c0dda397dca0233feeda203154864b3a20a
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-03-08 21:30:03 -0300

    Correct crouch collider

--- Assets/Scripts/Player/PlayerCrouch.cs
+++ Assets/Scripts/Player/PlayerCrouch.cs
@@ -9,9 +9,22 @@ namespace Player
     public class PlayerCrouch : NetworkBehaviour, ISpeedModifier
     {
         public event Action<bool> OnCrouchEvent;
+        
         [SerializeField] private InputReader inputReader;
+        [SerializeField] private CapsuleCollider capsuleCollider;
+        
         [SerializeField] private float speedModifier = 0.5f;
-    
+        
+        [Header("Crouch collider settings")]
+        [SerializeField] private Vector3 crouchColliderCenter;
+        [SerializeField] private float crouchRadius;
+        [SerializeField] private float crouchHeight;
+        
+        [Header("Stand collider settings")]
+        [SerializeField] private Vector3 standColliderCenter;
+        [SerializeField] private float standRadius;
+        [SerializeField] private float standHeight;
+        
         private bool _isCrouching;
         
         
@@ -28,10 +41,34 @@ namespace Player
         private void InputReader_OnCrouchEvent(bool isCrouching)
         {
             _isCrouching = isCrouching;
+
+            if (_isCrouching)
+            {
+                CrouchCollider();
+            }
+            else
+            {
+                StandCollider();
+            }
+            
             OnCrouchEvent?.Invoke(_isCrouching);
             
         }
 
+        private void CrouchCollider()
+        {
+            capsuleCollider.center = crouchColliderCenter;
+            capsuleCollider.radius = crouchRadius;
+            capsuleCollider.height = crouchHeight;
+        }
+        
+        private void StandCollider()
+        {
+            capsuleCollider.center = standColliderCenter;
+            capsuleCollider.radius = standRadius;
+            capsuleCollider.height = standHeight;
+        }
+        
         public float ModifySpeed(float baseSpeed)
         {
             if (_isCrouching)

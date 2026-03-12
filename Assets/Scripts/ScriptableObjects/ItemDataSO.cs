6927db94a2c3edb2e0aa1058fbd514bf1fd7ee72
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-03-12 18:04:42 -0300

    Start interaction system

---
+++ Assets/Scripts/ScriptableObjects/ItemDataSO.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "New Item", menuName = "ScriptableObjects/Game/ItemData")]
+    public class ItemDataSO : ScriptableObject
+    {
+        public int itemId;
+        public string itemName;
+        public Sprite icon;
+        
+    }
+}
+

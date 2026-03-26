53f88759ccc2fb63e2f4e7179c38d904bb54667b
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-03-25 22:40:33 -0300

    Functions for filter lobby

--- Assets/Scripts/Network/Lobby.cs
+++ Assets/Scripts/Network/Lobby.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Generic;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Lobbies;
 using Unity.Services.Lobbies.Models;
+using Unity.Services.Multiplayer;
 using UnityEngine;
 
 namespace Network
@@ -10,6 +12,11 @@ namespace Network
     public class Lobby : MonoBehaviour
     {
         private const int MAX_PLAYERS = 4;
+        private Unity.Services.Lobbies.Models.Lobby _hostLobby;
+        
+        private float _heartBeatTimer;
+        private float _heartBeatMaxTimer = 15f;
+        
         
         private async void Start()
         {
@@ -21,15 +28,37 @@ namespace Network
             
             await AuthenticationService.Instance.SignInAnonymouslyAsync(); // Change this for steam after
             
-            
         }
-        
+
+        private void Update()
+        {
+            HeartBeat();
+        }
+
+        private async void HeartBeat()
+        {
+            if (_hostLobby != null)
+            {
+                _heartBeatTimer -= Time.deltaTime;
+                
+                if (_heartBeatTimer <= 0f)
+                {
+                    _heartBeatTimer = _heartBeatMaxTimer;
+                    await LobbyService.Instance.SendHeartbeatPingAsync(_hostLobby.Id);
+                    
+                }
+                
+            }
+        }
+
         public async void CreateLobby()
         {
             try
             {
                 string lobbyName = "Lobby";
                 Unity.Services.Lobbies.Models.Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, MAX_PLAYERS);
+                _hostLobby = lobby;
+                
                 Debug.Log($"Lobby created:{lobby}");
                 
             }
@@ -38,7 +67,33 @@ namespace Network
                 Debug.Log(exception);
             }
         }
-        
+
+        public async void ListLobbies()
+        {
+            try
+            {
+                QueryLobbiesOptions options = new QueryLobbiesOptions
+                {
+                    Count = 25,
+                    Filters = new List<QueryFilter>
+                    {
+                        new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT)
+
+                    },
+                    Order = new List<QueryOrder>
+                    {
+                        new QueryOrder(false, QueryOrder.FieldOptions.Created)
+                    }
+                };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+
+        }
     }
 }
 

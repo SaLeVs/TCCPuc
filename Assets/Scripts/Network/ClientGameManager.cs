741f3febc890f4efb8899a8240dc104c23dbae70
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-03-29 17:15:23 -0300

    Joining relay

--- Assets/Scripts/Network/ClientGameManager.cs
+++ Assets/Scripts/Network/ClientGameManager.cs
@@ -1,6 +1,12 @@
+using System;
 using System.Threading.Tasks;
 using Systems;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
 using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 
 namespace Network
@@ -8,6 +14,9 @@ namespace Network
     public class ClientGameManager
     {
         private const int MAX_TRIES_TO_AUTH = 5;
+        
+        private JoinAllocation _allocation;
+        
         
         public async Task<bool> InitAsync()
         {
@@ -29,8 +38,36 @@ namespace Network
             Loader.Load(Loader.Scene.MainMenu);
         }
 
-        public async Task StartClientAsync()
+        public async Task StartClientAsync(string joinCode)
         {
+            try
+            { 
+                _allocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+                
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+                return;
+            }
+            
+            if (NetworkManager.Singleton.TryGetComponent(out UnityTransport transport))
+            {
+                RelayServerData relayData = new RelayServerData(
+                    _allocation.RelayServer.IpV4,
+                    (ushort)_allocation.RelayServer.Port,
+                    _allocation.AllocationIdBytes,
+                    _allocation.Key,
+                    _allocation.ConnectionData,
+                    _allocation.ConnectionData,
+                    true
+                );
+                
+                transport.SetRelayServerData(relayData);
+                
+                NetworkManager.Singleton.StartClient();
+            }
+            
             
         }
         

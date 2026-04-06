2b4bd59b46e3e0bfb9f01368847457c0c24c1ead
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-04-05 22:09:56 -0300

    Player can interact in totem

--- Assets/Scripts/Systems/ReadyTotem.cs
+++ Assets/Scripts/Systems/ReadyTotem.cs
@@ -9,44 +9,64 @@ namespace Systems
         [SerializeField] private PlayersReady playersReady;
         [SerializeField] private GameObject totemVisual;
 
-        private ulong _ownerClientId;
-        private bool _isActivated;
-        private bool _isEnabled;
+        private NetworkVariable<ulong> _ownerClientId = new NetworkVariable<ulong>();
+        private NetworkVariable<bool> _isEnabled = new NetworkVariable<bool>();
+        private NetworkVariable<bool> _isActivated = new NetworkVariable<bool>();
 
         
         public override void OnNetworkSpawn()
         {
             totemVisual.SetActive(false);
+            _isEnabled.OnValueChanged += ReadyTotem_IsEnabled;
             
         }
-        
+
+        private void ReadyTotem_IsEnabled(bool previousValue, bool newValue)
+        {
+            totemVisual.SetActive(newValue);
+        }
+
         public void AssignToPlayer(ulong clientId)
         {
-            _ownerClientId = clientId;
+            _ownerClientId.Value = clientId;
         }
 
         public void Activate()
         {
-            _isEnabled = true;
-            totemVisual.SetActive(_isEnabled);
-        }
-
-        public bool CanInteract(GameObject interactor)
-        {
-            if (!_isEnabled || _isActivated) return false;
-            if (!interactor.TryGetComponent(out NetworkObject netObj)) return false;
-            
-            return netObj.OwnerClientId == _ownerClientId;
+            _isEnabled.Value = true;
         }
 
         public bool Interact(GameObject playerInteractor)
         {
-            if (!CanInteract(playerInteractor)) return false;
+            if (!playerInteractor.TryGetComponent(out NetworkObject netObj)) return false;
 
-            _isActivated = true;
-            playersReady.SetPlayerReady();
-            Debug.Log($"Player {playerInteractor.name} activated their totem!");
+            InteractServerRpc(netObj);
             return true;
+        }
+
+        [Rpc(SendTo.Server)]
+        private void InteractServerRpc(NetworkObjectReference playerRef)
+        {
+            if (!playerRef.TryGet(out NetworkObject playerNetworkObject)) return;
+
+            if (!CanInteract(playerNetworkObject.gameObject)) return;
+
+            _isActivated.Value = true;
+            playersReady.SetPlayerReady();
+        }
+        
+        public bool CanInteract(GameObject interactor)
+        {
+            if (!_isEnabled.Value || _isActivated.Value) return false;
+            if (!interactor.TryGetComponent(out NetworkObject netObj)) return false;
+
+            return netObj.OwnerClientId == _ownerClientId.Value;
+        }
+        
+        public override void OnNetworkDespawn()
+        {
+            _isEnabled.OnValueChanged -= ReadyTotem_IsEnabled;
+            
         }
         
     } 

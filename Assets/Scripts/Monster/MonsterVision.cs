c6ef58b291cc90e69c0965e0dc6c8d363cbbbfbd
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-04-09 11:39:55 -0300

    Start monster vision

---
+++ Assets/Scripts/Monster/MonsterVision.cs
@@ -0,0 +1,103 @@
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Monster
+{
+    public class MonsterVision : NetworkBehaviour
+    {
+        [SerializeField] private float distance;
+        [SerializeField] private float angle;
+        [SerializeField] private float height;
+        
+        private Color meshColor = Color.red;
+        private Mesh mesh;
+        
+        private const float GIZMOS_ALPHA = 0.4f;
+
+
+        private void OnValidate()
+        {
+            mesh = CreateMesh();
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (mesh)
+            {
+                Gizmos.color = new Color(meshColor.r, meshColor.g, meshColor.b, GIZMOS_ALPHA);
+                Gizmos.DrawMesh(mesh, transform.position, transform.rotation);
+            }
+        }
+        
+        private Mesh CreateMesh()
+        {
+            Mesh mesh = new Mesh();
+
+            int triangles = 8;
+            int vertices = triangles * 3;
+
+            Vector3[] totalVertices = new Vector3[vertices];
+            int[] totalTriangles = new int[vertices];
+            
+            Vector3 bottomCenter = Vector3.zero;
+            Vector3 bottomLeft = Quaternion.Euler(0, -angle, 0) * Vector3.forward * distance;
+            Vector3 bottomRight = Quaternion.Euler(0, angle, 0) * Vector3.forward * distance;
+            
+            Vector3 topCenter = bottomCenter + Vector3.up * height;
+            Vector3 topRight = bottomRight + Vector3.up * height;
+            Vector3 topLeft =  bottomLeft + Vector3.up * height;
+
+            int vertexCount = 0;
+            
+            // Left side
+            totalVertices[vertexCount++] = bottomCenter;
+            totalVertices[vertexCount++] = bottomLeft;
+            totalVertices[vertexCount++] = topLeft;
+            
+            totalVertices[vertexCount++] = topLeft;
+            totalVertices[vertexCount++] = topCenter;
+            totalVertices[vertexCount++] = bottomCenter;
+            
+            // Right side
+            totalVertices[vertexCount++] = bottomCenter;
+            totalVertices[vertexCount++] = topCenter;
+            totalVertices[vertexCount++] = topRight;
+            
+            totalVertices[vertexCount++] = topRight;
+            totalVertices[vertexCount++] = bottomRight;
+            totalVertices[vertexCount++] = bottomCenter;
+            
+            // Far side
+            totalVertices[vertexCount++] = bottomLeft;
+            totalVertices[vertexCount++] = bottomRight;
+            totalVertices[vertexCount++] = topRight;
+            
+            totalVertices[vertexCount++] = topRight;
+            totalVertices[vertexCount++] = topLeft;
+            totalVertices[vertexCount++] = bottomLeft;
+
+            // Top
+            totalVertices[vertexCount++] = topCenter;
+            totalVertices[vertexCount++] = topLeft;
+            totalVertices[vertexCount++] = topRight;
+            
+            // Bottom
+            totalVertices[vertexCount++] = bottomCenter;
+            totalVertices[vertexCount++] = bottomRight;
+            totalVertices[vertexCount++] = bottomLeft;
+
+            for (int i = 0; i < vertices; ++i)
+            {
+                totalTriangles[i] = i;
+            }
+            
+            mesh.vertices = totalVertices;
+            mesh.triangles = totalTriangles;
+            mesh.RecalculateNormals();
+
+            return mesh;
+        }
+        
+    }
+    
+}

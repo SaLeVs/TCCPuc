6631f295ff473b6673ff1fc0346b4f43980850a2
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-04-09 12:17:05 -0300

    Scan targets in vision

--- Assets/Scripts/Monster/MonsterVision.cs
+++ Assets/Scripts/Monster/MonsterVision.cs
@@ -1,38 +1,104 @@
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
 namespace Monster
 {
+    [ExecuteInEditMode] // TEST
     public class MonsterVision : NetworkBehaviour
     {
+        [Header("Vision Settings")]
         [SerializeField] private float distance;
         [SerializeField] private float angle;
         [SerializeField] private float height;
-
         [SerializeField] private Color meshColor;
         
-        private Mesh mesh;
+        [Header("Scan Settings")]
+        [SerializeField] private int scanFrequency;
+        [SerializeField] private LayerMask scanLayerMask;
+        
+        [SerializeField] private List<GameObject> detectedObjects = new List<GameObject>();
+        
+        
+        private Mesh _mesh;
+        
+        private Collider[] _colliders = new Collider[50];
+        private int _collidersCount;
+        private float _scanInterval;
+        private float _scanTimer;
 
-
-        private void OnValidate()
+        
+        private void Start()
         {
-            mesh = CreateMesh();
+            _scanInterval = 1 / scanFrequency;
         }
 
-        private void OnDrawGizmos()
+        private void Update()
         {
-            if (mesh)
+            ScanTimer();
+        }
+
+        private void ScanTimer()
+        {
+            _scanTimer -= Time.deltaTime;
+            
+            if (_scanTimer <= 0)
             {
-                Gizmos.color = meshColor;
-                Gizmos.DrawMesh(mesh, transform.position, transform.rotation);
+                _scanTimer += _scanInterval;
+                ScanTargets();
             }
+        }
+
+        private void ScanTargets()
+        {
+            _collidersCount = Physics.OverlapSphereNonAlloc(transform.position, distance, _colliders, scanLayerMask, QueryTriggerInteraction.Collide);
+            detectedObjects.Clear();
+
+            for (int i = 0; i < _collidersCount; i++)
+            {
+                GameObject objectDetected = _colliders[i].gameObject;
+
+                if (IsObjectInVision(objectDetected))
+                {
+                    detectedObjects.Add(objectDetected);
+                }
+            }
+        }
+
+        private bool IsObjectInVision(GameObject objectForTest)
+        {
+            Vector3 origin = transform.position;
+            Vector3 destination = objectForTest.transform.position;
+            Vector3 direction = destination - origin;
+
+            if (direction.y < 0 || direction.y > height)
+            {
+                return false;
+            }
+
+            direction.y = 0;
+            
+            float deltaAngle = Vector3.Angle(direction, transform.forward);
+
+            if (deltaAngle > angle)
+            {
+                return false;
+            }
+            
+            return true;
+        }
+        private void OnValidate()
+        {
+            _mesh = CreateMesh();
+            _scanInterval = 1 / scanFrequency;
         }
         
         private Mesh CreateMesh()
         {
             Mesh mesh = new Mesh();
 
-            int triangles = 8;
+            int segments = 10;
+            int triangles = (segments * 4) + 2 + 2;
             int vertices = triangles * 3;
 
             Vector3[] totalVertices = new Vector3[vertices];
@@ -66,24 +132,40 @@ namespace Monster
             totalVertices[vertexCount++] = bottomRight;
             totalVertices[vertexCount++] = bottomCenter;
             
-            // Far side
-            totalVertices[vertexCount++] = bottomLeft;
-            totalVertices[vertexCount++] = bottomRight;
-            totalVertices[vertexCount++] = topRight;
-            
-            totalVertices[vertexCount++] = topRight;
-            totalVertices[vertexCount++] = topLeft;
-            totalVertices[vertexCount++] = bottomLeft;
+            float currentAngle = -angle;
+            float deltaAngle = (angle * 2) / segments;
 
-            // Top
-            totalVertices[vertexCount++] = topCenter;
-            totalVertices[vertexCount++] = topLeft;
-            totalVertices[vertexCount++] = topRight;
+            for (int i = 0; i < segments; ++i)
+            {
+                bottomLeft = Quaternion.Euler(0, currentAngle, 0) * Vector3.forward * distance;
+                bottomRight = Quaternion.Euler(0, currentAngle + deltaAngle, 0) * Vector3.forward * distance;
+                
+                topRight = bottomRight + Vector3.up * height;
+                topLeft =  bottomLeft + Vector3.up * height;
+                
+                // Far side
+                totalVertices[vertexCount++] = bottomLeft;
+                totalVertices[vertexCount++] = bottomRight;
+                totalVertices[vertexCount++] = topRight;
             
-            // Bottom
-            totalVertices[vertexCount++] = bottomCenter;
-            totalVertices[vertexCount++] = bottomRight;
-            totalVertices[vertexCount++] = bottomLeft;
+                totalVertices[vertexCount++] = topRight;
+                totalVertices[vertexCount++] = topLeft;
+                totalVertices[vertexCount++] = bottomLeft;
+
+                // Top
+                totalVertices[vertexCount++] = topCenter;
+                totalVertices[vertexCount++] = topLeft;
+                totalVertices[vertexCount++] = topRight;
+            
+                // Bottom
+                totalVertices[vertexCount++] = bottomCenter;
+                totalVertices[vertexCount++] = bottomRight;
+                totalVertices[vertexCount++] = bottomLeft;
+                
+                currentAngle += deltaAngle;
+            }
+            
+            
 
             for (int i = 0; i < vertices; ++i)
             {
@@ -97,6 +179,29 @@ namespace Monster
             return mesh;
         }
         
+        private void OnDrawGizmos()
+        {
+            if (_mesh)
+            {
+                Gizmos.color = meshColor;
+                Gizmos.DrawMesh(_mesh, transform.position, transform.rotation);
+            }
+            
+            Gizmos.DrawWireSphere(transform.position, distance);
+            
+            for (int i = 0; i < _collidersCount; ++i)
+            {
+                Gizmos.DrawSphere(_colliders[i].transform.position, 0.2f);
+            }
+            
+            
+            Gizmos.color = Color.darkGreen;
+
+            foreach (GameObject obj in detectedObjects)
+            {
+                Gizmos.DrawSphere(obj.transform.position, 0.2f);
+            }
+        }
     }
     
 }

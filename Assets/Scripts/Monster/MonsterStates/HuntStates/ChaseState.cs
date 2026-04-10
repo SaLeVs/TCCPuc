eeae775bee7ee8fd98ef497e8dcbbbe232968d19
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-04-10 00:04:39 -0300

    Create all concrete states for HSM

---
+++ Assets/Scripts/Monster/MonsterStates/HuntStates/ChaseState.cs
@@ -0,0 +1,16 @@
+﻿using Monster.HSM;
+
+namespace Monster.MonsterStates.HuntStates
+{
+    public class ChaseState : State
+    {
+        private readonly MonsterBrain _monsterBrain;
+        
+        public ChaseState(StateMachine stateMachine, State parentState, MonsterBrain monsterBrain) : base(stateMachine, parentState)
+        {
+            _monsterBrain = monsterBrain;
+        }
+        
+        // protected override State GetTransitionState() { } 
+    }
+}
--- Assets/Scripts/Monster/MonsterStates/ParentStates/MonsterHunt.cs
+++ Assets/Scripts/Monster/MonsterStates/ParentStates/MonsterHunt.cs
@@ -1,19 +1,23 @@
 ﻿using Monster.HSM;
+using Monster.MonsterStates.HuntStates;
 
 namespace Monster.MonsterStates.ParentStates
 {
     public class MonsterHunt : State
     {
         private readonly MonsterBrain _monsterBrain;
+        public readonly ChaseState chaseState;
+        public readonly AttackState attackState;
         
         public MonsterHunt(StateMachine stateMachine, State parentState, MonsterBrain monsterBrain) : base(stateMachine, parentState)
         {
             _monsterBrain = monsterBrain;
+            
+            chaseState = new ChaseState(stateMachine, this, monsterBrain);
+            attackState = new AttackState(stateMachine, this, monsterBrain);
         }
     
-        protected override void OnEnter()
-        {
-                
-        }
+        protected override State GetInitialState() => chaseState;
+         // protected override State GetTransitionState() { }
     }
 }

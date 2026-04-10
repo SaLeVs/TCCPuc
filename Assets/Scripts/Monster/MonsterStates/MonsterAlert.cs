db5ed4e5448de38ce99dd32a87f84c7820afef7f
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-04-09 23:43:20 -0300

    Creating concrete states

---
+++ Assets/Scripts/Monster/MonsterStates/MonsterAlert.cs
@@ -0,0 +1,21 @@
+﻿using Monster.HSM;
+
+namespace Monster.MonsterStates
+{
+    public class MonsterAlert : State
+    {
+        private readonly MonsterBrain _monsterBrain;
+        
+            
+        public MonsterAlert(StateMachine stateMachine, State parentState, MonsterBrain monsterBrain) : base(stateMachine, parentState)
+        {
+            _monsterBrain = monsterBrain;
+        }
+    
+        protected override void OnEnter()
+        {
+                
+        }
+            
+    }
+}
---
+++ Assets/Scripts/Monster/MonsterStates/MonsterRoaming.cs
@@ -0,0 +1,22 @@
+﻿using Monster.HSM;
+
+namespace Monster.MonsterStates
+{
+    public class MonsterRoaming : State
+    {
+        private readonly MonsterBrain _monsterBrain;
+        public readonly WanderState wanderState;
+        public readonly 
+        
+        public MonsterRoaming(StateMachine stateMachine, State parentState ,MonsterBrain monsterBrain) : base(stateMachine, parentState)
+        {
+            _monsterBrain = monsterBrain;
+            
+        }
+
+        protected override void OnEnter()
+        {
+            
+        }
+    }
+}

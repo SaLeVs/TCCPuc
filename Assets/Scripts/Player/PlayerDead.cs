6212f1673bb4e3b18be0c157b2663f41b84628cb
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-04-20 14:08:11 -0300

    Player die and monster ignore

--- Assets/Scripts/Player/PlayerDead.cs
+++ Assets/Scripts/Player/PlayerDead.cs
@@ -10,6 +10,8 @@ namespace Player
         public event Action<bool> OnDeathEvent;
 
         [SerializeField] private Health playerHealth;
+        [SerializeField] private LayerMask deadLayer;
+        [SerializeField] private LayerMask aliveLayer;
 
         public bool IsDead => _isDead.Value;
         
@@ -24,19 +26,53 @@ namespace Player
             {
                 playerHealth.OnDie += PlayerHealth_OnDie;
             }
+            
+            ApplyLayerState(_isDead.Value);
+            
         }
-
+        
         
         private void PlayerHealth_OnDie(Health health)
         {
             _isDead.Value = true;
         }
-
+        
         private void PlayerDead_OnIsDeadChanged(bool previous, bool current)
         {
+            ApplyLayerState(current);
             OnDeathEvent?.Invoke(current);
         }
+        
+        private void ApplyLayerState(bool dead)
+        {
+            int layerIndex = LayerMaskToLayer(dead ? deadLayer : aliveLayer);
+            SetLayerRecursively(gameObject, layerIndex);
+        }
 
+        private void SetLayerRecursively(GameObject playerObject, int layer)
+        {
+            playerObject.layer = layer;
+
+            foreach (Transform child in playerObject.transform)
+            {
+                SetLayerRecursively(child.gameObject, layer);
+            }
+        }
+
+        private int LayerMaskToLayer(LayerMask mask)
+        {
+            int layer = mask.value;
+            int layerIndex = 0;
+
+            while (layer > 1)
+            {
+                layer >>= 1;
+                layerIndex++;
+            }
+
+            return layerIndex;
+        }
+        
         
         public override void OnNetworkDespawn()
         {

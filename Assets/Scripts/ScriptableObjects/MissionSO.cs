56e86ae9787fab37c1c5895eda330c135cb55b9b
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-04-28 20:07:42 -0300

    Create mission SO

---
+++ Assets/Scripts/ScriptableObjects/MissionSO.cs
@@ -0,0 +1,17 @@
+﻿using Enums;
+using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "New Mission", menuName = "ScriptableObjects/Game/Mission")]
+    public class MissionSO : ScriptableObject
+    {
+        public int[] requiredRoomIDs;
+        
+        public MissionObjectiveType objectiveType;
+        
+        public string missionName;
+        public string instructions;
+    }
+    
+}

a5ab47531069557b2a1a8ebdb4101bdc8ee1d0d9
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-04-28 20:28:05 -0300

    Contract so correct for get all rooms Required

--- Assets/Scripts/ScriptableObjects/ContractsSO.cs
+++ Assets/Scripts/ScriptableObjects/ContractsSO.cs
@@ -8,11 +8,34 @@ namespace ScriptableObjects
     {
         public string contractName;
     
+        public MissionSO mainMission;
+        public List<MissionSO> personalMissions;
+        
         public List<RoomDataSO> requiredRooms;
         public List<RoomDataSO> baseRooms;
         public List<RoomDataSO> lootRooms;
         
         
+        public List<RoomDataSO> GetAllRequiredRooms()
+        {
+            List<RoomDataSO> rooms = new List<RoomDataSO>(requiredRooms);
+            
+            foreach (MissionSO mission in personalMissions)
+            {
+                foreach (int roomID in mission.requiredRoomIDs)
+                {
+                    RoomDataSO matchRoom = 
+                        requiredRooms.Find(roomData => roomData.roomID == roomID) ?? baseRooms.Find(roomData => roomData.roomID == roomID);
+
+                    if (matchRoom != null && !rooms.Contains(matchRoom))
+                    {
+                        rooms.Add(matchRoom);
+                    }
+                }
+            }
+
+            return rooms;
+        }
     }
 }
 

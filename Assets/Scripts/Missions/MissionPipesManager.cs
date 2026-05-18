32f9ba96e77662d649949d0c50c22554fb13f2b6
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-05-18 11:58:27 -0300

    WIP on AudienceSystem

--- Assets/Scripts/Missions/MissionPipesManager.cs
+++ Assets/Scripts/Missions/MissionPipesManager.cs
@@ -1,12 +1,20 @@
 using System;
 using Interfaces;
+using Missions.PersonalMissions;
 using Unity.Netcode;
+using UnityEngine;
 
 namespace Missions
 {
     public class MissionPipesManager : NetworkBehaviour, IMissionSpawnable
     {
         public event Action OnSpawnCompleted;
+        
+        [SerializeField] private MissionOwnershipSelector ownershipSelector;
+        [SerializeField] private MissionCompleter missionCompleter;
+        
+        public bool IsComplete { get; private set; }
+        
     
         public void RequestSpawn()
         {

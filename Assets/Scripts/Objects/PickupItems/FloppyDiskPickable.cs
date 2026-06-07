ff500d8386a17923e619ce0201ec22f4671f47c7
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-06-07 18:49:26 -0300

    System for drop all items

---
+++ Assets/Scripts/Objects/PickupItems/FloppyDiskPickable.cs
@@ -0,0 +1,41 @@
+﻿using Interfaces;
+using Systems;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Objects.PickupItems
+{
+    public class FloppyDiskPickable : NetworkBehaviour, IInteractable
+    {
+        public bool CanInteract(GameObject interactor) => true;
+
+        public bool Interact(GameObject playerInteractor)
+        {
+            if (!playerInteractor.TryGetComponent(out NetworkObject netObj)) return false;
+
+            if (!IsServer)
+            {
+                InteractServerRpc(netObj);
+                return true;
+            }
+
+            TryGiveDisk(netObj);
+            return true;
+        }
+
+        [Rpc(SendTo.Server)]
+        private void InteractServerRpc(NetworkObjectReference playerRef)
+        {
+            if (!playerRef.TryGet(out NetworkObject playerNetObj)) return;
+            TryGiveDisk(playerNetObj);
+        }
+
+        private void TryGiveDisk(NetworkObject playerNetObj)
+        {
+            if (!playerNetObj.TryGetComponent(out PlayerDiskHolder diskHolder)) return;
+
+            diskHolder.GiveDisk();
+            NetworkObject.Despawn();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerDroppper.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using Player;
+using ScriptableObjects;
+using Systems;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerDropper : NetworkBehaviour
+    {
+        [SerializeField] private PlayerInventory playerInventory;
+        [SerializeField] private PlayerDiskHolder playerDiskHolder;
+        [SerializeField] private PlayerDead playerDead;
+        [SerializeField] private ItemListSO itemDatabase;
+        [SerializeField] private GameObject diskItem;
+
+        
+        public override void OnNetworkSpawn()
+        {
+            if (!IsServer) return;
+            
+            playerDead.OnDeathEvent += PlayerDead_OnPlayerDied;
+        }
+
+        
+        private void PlayerDead_OnPlayerDied(bool isDead)
+        {
+            if (!isDead) return;
+
+            DropInventory();
+            DropDisk();
+        }
+        
+        private void DropInventory()
+        {
+            List<int> items = playerInventory.CollectAndClearAllItems();
+
+            foreach (int itemId in items)
+            {
+                ItemDataSO item = itemDatabase.GetItem(itemId);
+                
+                if (item != null)
+                {
+                    SpawnPickable(item.prefabPickable);
+                }
+            }
+        }
+
+        private void DropDisk()
+        {
+            if (!playerDiskHolder.HasDisk) return;
+            if (diskItem == null) return;
+
+            playerDiskHolder.TryConsumeDisk();
+            SpawnPickable(diskItem);
+        }
+
+        private void SpawnPickable(GameObject prefab)
+        {
+            if (prefab == null) return;
+
+            Transform spawnPoint = LostItemsSpawn.Instance?.GetNextSpawnPoint();
+            if (spawnPoint == null) return;
+
+            GameObject spawned = Instantiate(prefab, spawnPoint.position, spawnPoint.rotation);
+
+            if (spawned.TryGetComponent(out NetworkObject netObj))
+            {
+                netObj.Spawn();
+            }
+        }
+
+        
+        public override void OnNetworkDespawn()
+        {
+            if (!IsServer) return;
+            
+            playerDead.OnDeathEvent -= PlayerDead_OnPlayerDied;
+        }
+        
+        
+    }
+}

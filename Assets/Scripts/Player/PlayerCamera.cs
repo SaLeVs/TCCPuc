9c9fb3c566ccc76af55e16361f6917b713a91041
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-08-31 20:32:29 -0300

    Improve cam movement

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -9,81 +9,72 @@ namespace Player
     public class PlayerCamera : NetworkBehaviour
     {
         public event Action<bool> OnPauseToggled;
-        
+
         [SerializeField] private PlayerState playerState;
         [SerializeField] private CinemachineCamera cinemachineCamera;
+        [SerializeField] private CinemachineInputAxisController inputAxisController;
+        [SerializeField] private CinemachinePanTilt panTilt;
         [SerializeField] private Transform cameraRoot;
         [SerializeField] private InputReader inputReader;
         [SerializeField] private Transform orientation;
         [SerializeField] private Renderer[] occlusionRenderers;
-        
-        [SerializeField] private int ownerCameraPriority = 10;
-        [SerializeField] private float upperClamp = -40f;
-        [SerializeField] private float lowerClamp = 70f;
-        [SerializeField] private float sensitivity = 20f;
-        
-        public CinemachineCamera playerCinemachineCamera => cinemachineCamera;
-        
-        private Vector2 _lookInput;
 
-        private float _yaw;
-        private float _pitch;
-        
+        [SerializeField] private int ownerCameraPriority = 10;
+
+        public CinemachineCamera playerCinemachineCamera => cinemachineCamera;
+
         private bool _isDead;
         private bool _isLocked;
         private bool _isPaused;
-        
-        
+
         public override void OnNetworkSpawn()
         {
             if (IsOwner)
             {
                 cinemachineCamera.Priority = ownerCameraPriority;
-                inputReader.OnCameraLookEvent += InputReader_OnCameraLookEvent;
                 inputReader.OnPauseEvent += ToggleMouse;
                 playerState.OnPlayerDead += PlayerState_OnPlayerDead;
                 playerState.OnPlayerLocked += PlayerState_OnPlayerLocked;
-                
+
+                inputAxisController.enabled = true;
                 LockMouse();
                 HideOcclusionRenderers();
-            } 
+            }
+            else
+            {
+                inputAxisController.enabled = false;
+            }
         }
 
-        
-        private void InputReader_OnCameraLookEvent(Vector2 cameraLookInput) => _lookInput = cameraLookInput;
-        
         public void SetSpectatorMode(bool isSpectating)
         {
             cinemachineCamera.Priority = isSpectating ? 0 : ownerCameraPriority;
         }
-        
+
         private void ToggleMouse()
         {
             _isPaused = !_isPaused;
+            inputAxisController.enabled = !_isPaused && !_isLocked;
 
-            if (_isPaused)
-            {
-                UnlockMouse();
-            }
-            else
-            {
-                LockMouse();
-            }
-            
+            if (_isPaused) UnlockMouse();
+            else LockMouse();
+
             OnPauseToggled?.Invoke(_isPaused);
         }
-        
+
         public void SetPauseState(bool isPaused)
         {
             _isPaused = isPaused;
+            inputAxisController.enabled = !_isPaused && !_isLocked;
             OnPauseToggled?.Invoke(isPaused);
         }
-        
+
         private void PlayerState_OnPlayerDead(bool isDead) => _isDead = isDead;
-        
+
         private void PlayerState_OnPlayerLocked(bool locked)
         {
             _isLocked = locked;
+            inputAxisController.enabled = !locked && !_isPaused && !_isDead;
 
             if (locked)
             {
@@ -92,14 +83,8 @@ namespace Player
             }
             else
             {
-                if (_isPaused)
-                {
-                    UnlockMouse();
-                }
-                else
-                {
-                    LockMouse();
-                }
+                if (_isPaused) UnlockMouse();
+                else LockMouse();
                 cinemachineCamera.Priority = ownerCameraPriority;
             }
         }
@@ -109,64 +94,50 @@ namespace Player
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
-        
+
         private void UnlockMouse()
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
-        
+
         private void HideOcclusionRenderers()
         {
             foreach (Renderer currentRenderer in occlusionRenderers)
-            {
                 currentRenderer.enabled = false;
-            }
         }
-        
+
         public void SetOcclusionRenderersVisible(bool visible)
         {
             foreach (Renderer currentRenderer in occlusionRenderers)
                 currentRenderer.enabled = visible;
         }
-        
+
         private void LateUpdate()
         {
             if (IsOwner && !_isDead && !_isLocked)
-            {   
-                CameraMovement();
-            } 
+            {
+                SyncBodyRotationWithCamera();
+            }
         }
         
-        private void CameraMovement()
+        private void SyncBodyRotationWithCamera()
         {
-            float deltaTime = Time.deltaTime;
-            
-            _yaw += _lookInput.x * sensitivity * deltaTime;
-            
-            _pitch -= _lookInput.y * sensitivity * deltaTime;
-            _pitch = Mathf.Clamp(_pitch, upperClamp, lowerClamp);
-            
-            orientation.rotation = Quaternion.Euler(0f, _yaw, 0f);
-            transform.rotation = Quaternion.Euler(0f, _yaw, 0f);
-            cameraRoot.localRotation = Quaternion.Euler(_pitch, 0f, 0f);
+            float yaw = panTilt.PanAxis.Value;
+            orientation.rotation = Quaternion.Euler(0f, yaw, 0f);
+            transform.rotation = Quaternion.Euler(0f, yaw, 0f);
         }
-        
-        
+
         public override void OnNetworkDespawn()
         {
             if (IsOwner)
             {
-                inputReader.OnCameraLookEvent -= InputReader_OnCameraLookEvent;
                 inputReader.OnPauseEvent -= ToggleMouse;
                 playerState.OnPlayerDead -= PlayerState_OnPlayerDead;
                 playerState.OnPlayerLocked -= PlayerState_OnPlayerLocked;
-                
+
                 cinemachineCamera.Priority = 0;
             }
         }
-        
     }
 }
-
-

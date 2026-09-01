6b963c43ce14d50b38fbc93860b876420031c01c
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-09-01 09:05:32 -0300

    Change visibility off player info

--- Assets/Scripts/Ui/PlayerInfosUi.cs
+++ Assets/Scripts/Ui/PlayerInfosUi.cs
@@ -8,11 +8,14 @@ namespace UI
 {
     public class PlayerInfosUi : MonoBehaviour
     {
+        [SerializeField] private Canvas canvas;
         [SerializeField] private PlayerState playerState;
         [SerializeField] private TextMeshProUGUI playerNameText;
 
         private void Start()
         {
+            canvas.enabled = !playerState.IsOwner;
+            
             PlayerInfos_OnPlayerNameChanged(string.Empty, playerState.PlayerInfos.PlayerName.Value);
             playerState.PlayerInfos.PlayerName.OnValueChanged += PlayerInfos_OnPlayerNameChanged;
         }

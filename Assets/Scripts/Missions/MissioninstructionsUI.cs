0bb2d43862aa0856232ba22a6e4fc40a5fd50950
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-09-17 08:31:35 -0300

    Correct mission holder

--- Assets/Scripts/Missions/MissioninstructionsUI.cs
+++ Assets/Scripts/Missions/MissioninstructionsUI.cs
@@ -1,16 +1,19 @@
 using ScriptableObjects;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MissioninstructionsUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI titleText;
     [SerializeField] private TextMeshProUGUI instructionsText;
+    [SerializeField] private Image missionImage;
 
     public void Setup(MissionSO mission)
     {
         titleText.text = mission.missionName;
         instructionsText.text = mission.instructions;
+        missionImage.sprite = mission.missionImage;
     }
     
 }
